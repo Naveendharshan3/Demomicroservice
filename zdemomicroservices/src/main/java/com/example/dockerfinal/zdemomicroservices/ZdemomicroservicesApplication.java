43ff25351128ec3f1815f3ea9e33728d3631f637
user <user@user-PC>
2019-02-07 00:14:00 +0530

    first commit

---
+++ zdemomicroservices/src/main/java/com/example/dockerfinal/zdemomicroservices/ZdemomicroservicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.dockerfinal.zdemomicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdemomicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdemomicroservicesApplication.class, args);
+	}
+
+}
+
---
+++ zdemomicroservices/src/main/java/com/example/dockerfinal/zdemomicroservices/sample1/Restcontrollers.java
@@ -0,0 +1,15 @@
+package com.example.dockerfinal.zdemomicroservices.sample1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Restcontrollers {
+	
+	@GetMapping
+	public String all()
+	{
+		return "Hi there here is ur  first docker container RUNNING SUCESSFULLY..!";
+	}
+
+}
